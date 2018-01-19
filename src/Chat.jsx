36e18293eb2b81326c7a5671f79b488a40c4599b
Sam Greenberg <smgrnbrg91495@gmail.com>
2018-01-19 10:02:32 -0500

    I got it working, but I'm using another site as the server to pass messages, but who cares

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import { Panel, FormControl } from 'react-bootstrap';
 import Message from './Message.jsx';
 import ReactDOM from 'react-dom';
+import mqtt from 'mqtt';
 
 class Chat extends Component {
 
   constructor({ chatname, name, props }) {
     super(props);
 
-    console.log(name);
+    this.client  = mqtt.connect('ws://test.mosquitto.org:8080/mqtt')
+
+    var self = this;
+
+    self.client.on('connect', function () {
+      self.client.subscribe('chat');
+      self.setState({loading: false});
+    })
+
+    self.client.on('message', function (topic, message) {
+      self.addMessage(JSON.parse(message.toString()));
+    })
 
     this.state = {
+      loading: true,
       chatname: chatname,
       username: name,
       curMessage: "",
@@ -53,15 +66,23 @@ class Chat extends Component {
 
   sendMessage() {
     if (this.state.curMessage !== "") {
-      let messages = [...this.state.messages];
-      messages.push({
-        name: this.state.username,
-        body: this.state.curMessage,
-        timestamp: Date.now()
-      });
+      // let messages = [...this.state.messages];
+      const newMessage = {
+        "name": this.state.username,
+        "body": this.state.curMessage,
+        "timestamp": Date.now()
+      };
 
-      this.setState({messages, curMessage: ""});
+      this.client.publish('chat', JSON.stringify(newMessage));
+
+      this.setState({curMessage: ""});
     }
+  }
+
+  addMessage(message) {
+    let messages = [...this.state.messages];
+    messages.push(message);
+    this.setState({messages});
   }
 
   componentDidMount() {
@@ -100,6 +121,7 @@ class Chat extends Component {
         <Panel.Footer>
           <FormControl componentClass="textarea"
             className="messages"
+            disabled={this.state.loading}
             onChange={(event) => {this.setCurMessage(event.target.value)}}
             onKeyDown={(event) => {this.handleKeyDown(event)}}
             onSubmit={() => {this.sendMessage()}}
--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+var mqtt = require('mqtt')
+var client  = mqtt.connect('ws://test.mosquitto.org:8080/mqtt')
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
